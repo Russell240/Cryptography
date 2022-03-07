c7b2d85678b0fee156ed37f2fb03b3e803929024
Russell Flaherty <flahertypro1@outlook.com>
2022-03-07 21:13:17 +0000

    instance of signature in signing app

---
+++ RandomizingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using CryptographyLib;
+using System;
+namespace RandomizingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How Big do you want the key in the bytes ");
+            string size = Console.ReadLine();
+            byte[] key = Protector.GetRandomKeyOrIV(int.Parse(size));
+            Console.WriteLine($"Key as byte array");
+            for (int b = 0; b < key.Length; b++) 
+            {
+                Console.WriteLine($"{key[b]:x2}");
+                if (((b + 1) % 16) == 0) Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ SigningApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CryptographyLib;
+
+namespace SigningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string data = Console.ReadLine();
+            var signature = Protector.GenerateSignature(data);
+            Console.WriteLine($"Signature :{signature}");
+            Console.WriteLine("protector", Protector.PublicKey);
+        }
+    }
+}
